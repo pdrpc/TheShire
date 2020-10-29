ff7cfdcb3af399a55054f0270b7436199a4f39e4
DeGilmour <49701510+DeGilmour@users.noreply.github.com>
2020-10-29 16:57:55 -0300

    LoginComponentV1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LembretesComponent } from './lembretes/lembretes.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LembretesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
