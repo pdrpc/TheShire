2c4334769c1d0a29a446c885806061ea62981cce
RLemonache <49251607+RLemonache@users.noreply.github.com>
2020-10-30 20:45:41 -0300

    scriptsSQL

---
+++ sql/lembretes_db.sql
@@ -0,0 +1,17 @@
+create database lembretes_db;
+create table users(
+	user_ID int not null auto_increment unique,
+    user_nick varchar(64) not null,
+    user_name varchar(64), 
+    user_mail varchar(360),
+    user_pass varchar(64),
+    primary key (user_ID)
+);
+create table lembretes(
+	lmbrt_nome varchar(255) not null,
+    data_criar date not null,
+    data_final date,
+    lmbrt_body varchar(1024),
+    user_ID int not null,
+    foreign key (user_ID) references users(user_ID)
+);
