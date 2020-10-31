9bd579a44fdf33f309e772c128a141749daaef9b
Kagnot <luizcagnotodeveloper@gmail.com>
2020-10-31 12:44:08 -0300

    classe inserir ver 1.00

--- src/app/lembretes/inserir-lembretes/inserir-lembretes.component.ts
+++ src/app/lembretes/inserir-lembretes/inserir-lembretes.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { lembretesService } from '../lembretes.service';
 
 @Component({
   selector: 'app-inserir-lembretes',
   templateUrl: './inserir-lembretes.component.html',
   styleUrls: ['./inserir-lembretes.component.css']
 })
-export class InserirLembretesComponent implements OnInit {
+export class InserirLembretesComponent{
 
-  constructor() { }
+  constructor(public lembretesService: lembretesService) {
 
-  ngOnInit(): void {
   }
 
+    onAdicionarLembretes(form: NgForm) {
+    if (form.invalid) return;
+    this.lembretesService.adicionarLembrete(
+      form.value.titulo,
+      // aqui eu real n sei:-: form.value.getDate(),
+      form.value.data,
+      form.value.body;
+    );
+    form.resetForm();
+  }
 }
---
+++ src/app/lembretes/lembretes.model.ts
@@ -0,0 +1,6 @@
+export interface lembretes {
+  titulo: string;
+  data_cad: Date;
+  data_atv: Date;
+  body: String;
+}
