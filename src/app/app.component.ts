bde66fc15ebd083749b4f1f6f1f043a61e905af6
Kagnot <luizcagnotodeveloper@gmail.com>
2020-11-01 16:11:34 -0300

    lisa ver 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Lembrete } from './lembretes/lembrete.model'
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projetoLembretes';
+  lembretes: Lembrete[] = [];
+  onLembreteAdicionado(Lembrete){
+    this.lembretes = [...this.lembretes, Lembrete];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { AppComponent } from './app.component';
 import { LembretesListaComponent } from './lembretes/lembretes-lista/lembretes-lista.component';
 import { InserirLembretesComponent } from './lembretes/inserir-lembretes/inserir-lembretes.component';
@@ -13,7 +18,13 @@ import { InserirLembretesComponent } from './lembretes/inserir-lembretes/inserir
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
